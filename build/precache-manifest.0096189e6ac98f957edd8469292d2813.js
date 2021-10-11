fa0b4d3fa3fe08bf6ae004be933ca7d4146bd35d
Joseph Tyler Buenaventura <jbuenaventura@mail.sfsu.edu>
2021-10-11 15:23:42 -0700

    Build run

---
+++ build/precache-manifest.0096189e6ac98f957edd8469292d2813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fa80c6cfa513a87ef2e52d62d33b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c25b35da24f8a8ac59e",
+    "url": "/static/css/main.161bdc24.chunk.css"
+  },
+  {
+    "revision": "4a2338d59362c587946a",
+    "url": "/static/js/2.7d7eef4c.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.7d7eef4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c25b35da24f8a8ac59e",
+    "url": "/static/js/main.763b60ef.chunk.js"
+  },
+  {
+    "revision": "7fbea2a6c92de49844e7",
+    "url": "/static/js/runtime-main.0d307ae0.js"
+  }
+]);
